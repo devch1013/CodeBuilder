3099cec51458cb23043d7dd78946e03c65ceb670
ChanHyuk Park <devch1013@gmail.com>
2024-09-18 01:07:41 +0900

    codeBuilder 분기 추가

--- src/code_builder.py
+++ src/code_builder.py
@@ -2,9 +2,10 @@ import os
 
 import dotenv
 import anthropic
+import subprocess
 
 from src.file.file_watcher import FileWatcher
-from step_manager import StepManager, StepType
+from src.step_manager import StepManager, StepType
 from src.llm.llm import LLM, Claude
 
 dotenv.load_dotenv(".env")
@@ -18,19 +19,27 @@ class CodeBuilder:
         self.file_watcher = FileWatcher(self.current_dir)
         
     def do_next(self):
-        step_type, step = self.step_manager.get_next()
+        next_step = self.step_manager.get_next()
+        step_type, step = next_step
         if step_type == StepType.CD:
+            print("cd", step)
             self.file_watcher.change_dir(step)
+            os.chdir(step)
         elif step_type == StepType.COMMAND:
-            os.system(step)
+            print("command", step)
+            subprocess.run(step, shell=True)
         elif step_type == StepType.MODIFY:
-            print("modify")
+            print("modify", step)
         elif step_type == StepType.CREATE:
-            print("create")
+            print("create", step)
         
     def start(self, query):
-        self.step_manager.get_step(query)
+        self.step_manager.get_fake_step(query)
+        while self.step_manager.check_step():
+            self.do_next()
+            
         
 if __name__ == "__main__":
     code_builder = CodeBuilder()
     print(code_builder.current_dir)
+    code_builder.start("Generate a new django project about memo app")
--- src/step_manager.py
+++ src/step_manager.py
@@ -14,31 +14,56 @@ class StepType(Enum):
     MODIFY = "modify"
     CREATE = "create"
 
+
 class StepManager:
     def __init__(self, llm: LLM):
         self.llm = llm
         self.steps: List[List[str]] = None
-        
+
     def get_step(self, query):
-        start_token = "[\n[\""
+        start_token = '[\n["'
         result = self.llm.run(prompt.start_project, query, start_token)
         print("result: ", result)
         step_string = start_token + result
         self.steps = extract_list_from_string(step_string)
         return self.steps
-    
+
+    def get_fake_step(self, query):
+        self.steps = [
+            ["command", "django-admin startproject memo_project"],
+            ["command", "cd memo_project"],
+            ["command", "python manage.py startapp memo"],
+            ["modify", "./memo_project/settings.py"],
+            ["modify", "./memo_project/urls.py"],
+            ["create", "./memo/models.py"],
+            ["create", "./memo/views.py"],
+            ["create", "./memo/urls.py"],
+            ["create", "./memo/forms.py"],
+            ["create", "./memo/templates/memo/index.html"],
+            ["command", "python manage.py makemigrations"],
+            ["command", "python manage.py migrate"],
+            ["command", "python manage.py runserver"],
+        ]
+
+    def check_step(self):
+        if self.steps is [] or self.steps is None:
+            return False
+        return True
+
     def get_next(self) -> Tuple[StepType, str]:
+        if self.steps is [] or self.steps is None:
+            return None
         next_step = self.steps.pop(0)
+        print("next_step: ", next_step)
+
         if next_step[0] == StepType.MODIFY.value:
             return StepType.MODIFY, next_step[1]
         if next_step[0] == StepType.CREATE.value:
             return StepType.CREATE, next_step[1]
         if next_step[0] == StepType.COMMAND.value:
             if next_step[1].split()[0] == StepType.CD.value:
-                return StepType.CD, next_step[2]
+                return StepType.CD, next_step[1].split()[1]
         return StepType.COMMAND, next_step[1]
-        
-
 
 
 if __name__ == "__main__":
@@ -46,6 +71,3 @@ if __name__ == "__main__":
     step_manager = StepManager(llm)
     step_manager.get_step("Generate a new django project about memo app")
     print(step_manager.steps)
-    
-        
-        
